b5de65fa03ccd7784f8a50e36649ee16e0224438
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-09-12 14:52:07 +0800

    setup basic app using express

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there' })
+})
+
+const PORT = process.env.PORT || 5000
+const message = `app listening to ${PORT}`
+
+app.listen(PORT, console.log(message))
