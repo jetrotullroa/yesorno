58e618b9ea0ed281727f82fea080eb5da86a3f08
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-09-13 10:15:52 +0800

    finish auth flow. separate keys of prod and dev

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  googleClientID: '33281786834-pve9n5ov132kfno86t4qd8452pg4ugkh.apps.googleusercontent.com',
+  googleClientSecret: 'zo7kfgmQLHKxH1OGJugXqwIV',
+  mongoURI: 'mongodb://emaily_dev:password@ds135234.mlab.com:35234/emaily-dev',
+  cookieKeys: `qEmaily$8765432`
+}
