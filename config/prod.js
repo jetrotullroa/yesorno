e9eb1415ae768eb991a76d94cdaec96634fc387f
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-09-30 15:49:15 +0800

    setup email sending and redirect

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
   cookieKeys: process.env.COOKIE_KEYS,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY
+  sendGridKey: process.env.SEND_GRID_KEY,
+  redirectURL: process.env.REDIRECT_URL
 }
