dfcb2251de4d3c803d7e409fd5aacc71feb88aba
Jetro Tull Roa <jetrotullroa@gmail.com>
2017-10-02 15:22:41 +0800

    add validation to form

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,20 +1,20 @@
 import React, { Component } from 'react'
 import { reduxForm, Field } from 'redux-form'
 import { Link } from 'react-router-dom'
+import validateEmail from '../../utils/validateEmail'
 
 import SurveyField from './SurveyField'
 
+const FIELDS = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipients List', name: 'recipients' },
+]
 
 class SurveyForm extends Component {
 
   renderField() {
-
-    const FIELDS = [
-      { label: 'Survey Title', name: 'title' },
-      { label: 'Subject Line', name: 'subject' },
-      { label: 'Email Body', name: 'body' },
-      { label: 'Recipients List', name: 'recipients' },
-    ]
 
     return FIELDS.map( ({label, name}, index) => {
       return(
@@ -31,10 +31,10 @@ class SurveyForm extends Component {
           onSubmit={this.props.handleSubmit(values => console.log(values))}
         >
           {this.renderField()}
-          <div className="buttons">
+          <div className="buttons" style={{ marginTop: '15px' }}>
             <Link to="/surveys" className="red btn-flat white-text">Cancel</Link>
             <button type="submit" className="grey btn-flat right white-text">
-            Submit
+            Review
             <i className="material-icons right">done</i>
             </button>
           </div>
@@ -43,7 +43,22 @@ class SurveyForm extends Component {
     )
   }
 }
+function validate(values) {
+  const errors = {}
+
+  errors.recipients = validateEmail(values.recipients || '')
+
+  FIELDS.forEach( ({ name }) => {
+    if (!values[name]) {
+      errors[name] = "You must provide a value"
+    }
+  })
+
+
+  return errors
+}
 
 export default reduxForm({
+  validate,
   form: 'surveyForm'
 })(SurveyForm)
